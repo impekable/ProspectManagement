b5efa8405f94966c17d8c0face6b0f0859ef4c18
danszot22 <danszot@hotmail.com>
2017-07-28 16:13:53 -0400

    Add missing file.

---
+++ ProspectManagement/Interfaces/Repositories/IProspectCache.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using ProspectManagement.Core.Models;
+
+namespace ProspectManagement.Core.Interfaces.Repositories
+{
+    public interface IProspectCache
+    {
+        Prospect GetProspectFromCache(int id);
+        void SaveProspectToCache(Prospect prospect);
+    }
+}
