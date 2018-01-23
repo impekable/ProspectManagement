5aa606994a6fc5f77fd54a24af5f780e7e08fd03
danszot22 <danszot@hotmail.com>
2018-01-23 16:10:22 -0500

    Fix pre build script.

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -3,5 +3,5 @@ KEYPATH="${APPCENTER_SOURCE_DIRECTORY:-.}"
 echo "${KEYPATH}/ProspectManagement/Constants/PrivateKeys.cs"
 echo "${CLIENT_ID}"
 echo "${REST_RESOURCE_NAME}"
-sed -i '' "s/REST_RESOURCE_NAME/${REST_RESOURCE_NAME}/g" $KEYPATH/Constants/PrivateKeys.cs
-sed -i '' "s/CLIENT_ID/${CLIENT_ID}/g" $KEYPATH/Constants/PrivateKeys.cs
+sed -i '' "s/REST_RESOURCE_NAME/${REST_RESOURCE_NAME}/g" $KEYPATH/ProspectManagement/Constants/PrivateKeys.cs
+sed -i '' "s/CLIENT_ID/${CLIENT_ID}/g" $KEYPATH/ProspectManagement/Constants/PrivateKeys.cs
