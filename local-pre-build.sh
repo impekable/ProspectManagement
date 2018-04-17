d27bbf19e366142a980455a10b4ee89be9e4f5a7
danszot22 <danszot@hotmail.com>
2018-04-17 15:47:56 -0400

    Change build scripts to use internal API url.

--- local-pre-build.sh
+++ local-pre-build.sh
@@ -20,20 +20,20 @@ elif [ "$1" == "DEV" ]; then
     #plutil -replace CFBundleIdentifier -string com.khov.prospects.dev ../iOS/info.plist
     plutil -replace XSAppIconAssets -string Resources/Assets.xcassets/AppIcons-DEV.appiconset ../iOS/info.plist
 elif [ "$1" == "PY" ]; then
-    echo "Changing REST endpoint to mobilitye1py.khov.com/PY910"
-    sed -i '' "s#REST_ENDPOINT#mobilitye1py.khov.com/PY910#g" ../ProspectManagement/Constants/ConnectionURIs.cs
+    echo "Changing REST endpoint to bssve1py.khov.com/PY910"
+    sed -i '' "s#https://REST_ENDPOINT#http://bssve1py.khov.com/PY910#g" ../ProspectManagement/Constants/ConnectionURIs.cs
     plutil -replace CFBundleDisplayName -string "Prospects PY" ../iOS/info.plist
     #plutil -replace CFBundleIdentifier -string com.khov.prospects.py ../iOS/info.plist
     plutil -replace XSAppIconAssets -string Resources/Assets.xcassets/AppIcons-PY.appiconset ../iOS/info.plist
 elif [ "$1" == "TRN" ]; then
-    echo "Changing REST endpoint to mobilitye1tr.khov.com/TR910"
-    sed -i '' "s#REST_ENDPOINT#mobilitye1tr.khov.com/TR910#g" ../ProspectManagement/Constants/ConnectionURIs.cs
+    echo "Changing REST endpoint to bssve1tr.khov.com/TR910"
+    sed -i '' "s#https://REST_ENDPOINT#http://bssve1tr.khov.com/TR910#g" ../ProspectManagement/Constants/ConnectionURIs.cs
     plutil -replace CFBundleDisplayName -string "Prospects TRN" ../iOS/info.plist
     #plutil -replace CFBundleIdentifier -string com.khov.prospects.trn ../iOS/info.plist
     plutil -replace XSAppIconAssets -string Resources/Assets.xcassets/AppIcons-TRN.appiconset ../iOS/info.plist
 elif [ "$1" == "PROD" ]; then
-    echo "Changing REST endpoint to mobilitye1pd.khov.com/PD910"
-    sed -i '' "s#REST_ENDPOINT#mobilitye1pd.khov.com/PD910#g" ../ProspectManagement/Constants/ConnectionURIs.cs
+    echo "Changing REST endpoint to bssve1pd.khov.com/PD910"
+    sed -i '' "s#https://REST_ENDPOINT#http://bssve1pd.khov.com/PD910#g" ../ProspectManagement/Constants/ConnectionURIs.cs
     plutil -replace CFBundleDisplayName -string "Prospects" ../iOS/info.plist
     #plutil -replace CFBundleIdentifier -string com.khov.prospects ../iOS/info.plist
     plutil -replace XSAppIconAssets -string Resources/Assets.xcassets/AppIcons.appiconset ../iOS/info.plist
