9faa3d5b5c108c5779e7c2bc8c087174a3853c39
danszot22 <danszot@hotmail.com>
2018-05-09 10:05:00 -0400

    Reset Private Keys.

---
+++ ProspectManagement/Constants/PrivateKeys.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace ProspectManagement.Core.Constants
+{
+    public static class PrivateKeys
+    {
+        public const string ProspectMgmtRestResource = @"https://khov.com/REST_RESOURCE_NAME";         public const string ProspectMgmtClientId = @"CLIENT_ID";
+    }
+}
