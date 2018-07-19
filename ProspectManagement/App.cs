f988b3f4499916ce588bc9e1c3ae176a2ce65b0d
danszot22 <danszot@hotmail.com>
2018-07-19 09:25:12 -0400

    Upgrade to MVVMCross V6.

---
+++ ProspectManagement/App.cs
@@ -0,0 +1,29 @@
+﻿using MvvmCross.ViewModels;
+using MvvmCross.IoC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProspectManagement.Core.ViewModels;
+
+namespace ProspectManagement.Core
+{
+    public class App: MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Repository")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+			//RegisterCustomAppStart<AppStart>();
+			RegisterAppStart<RootViewModel>();
+        }
+    }
+}
---
+++ ProspectManagement/AppStart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MvvmCross.Navigation;
+using MvvmCross.ViewModels;
+using ProspectManagement.Core.ViewModels;
+
+namespace ProspectManagement.Core
+{
+	public class AppStart: MvxAppStart
+    {
+		private readonly IMvxNavigationService _mvxNavigationService;
+
+		public AppStart(IMvxApplication app, IMvxNavigationService mvxNavigationService): base(app, mvxNavigationService)
+        {
+			_mvxNavigationService = mvxNavigationService;
+
+        }
+
+		protected override void NavigateToFirstViewModel(object hint = null)
+		{
+			_mvxNavigationService.Navigate<RootViewModel>();
+		}
+	}
+}
