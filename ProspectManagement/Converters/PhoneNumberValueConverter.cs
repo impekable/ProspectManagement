3d6be05efa92ea4a96fd737ec0fddd7cfc4ed89d
danszot22 <danszot@hotmail.com>
2018-10-04 16:04:59 -0400

    Allow for international phone numbers.

--- ProspectManagement/Converters/PhoneNumberValueConverter.cs
+++ ProspectManagement/Converters/PhoneNumberValueConverter.cs
@@ -9,14 +9,21 @@ namespace ProspectManagement.Core.Converters
 	{
 		public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
 		{
-			var numbers = Regex.Replace(value.ToString(), @"\D", "");
+			if (value.ToString().StartsWith("+"))
+			{
+				return value;
+			}
+			else
+			{
+				var numbers = Regex.Replace(value.ToString(), @"\D", "");
 
-			if (numbers.Length <= 3)
-				return numbers;
-			if (numbers.Length <= 7)
-				return string.Format("{0}-{1}", numbers.Substring(0, 3), numbers.Substring(3));
+				if (numbers.Length <= 3)
+					return numbers;
+				if (numbers.Length <= 7)
+					return string.Format("{0}-{1}", numbers.Substring(0, 3), numbers.Substring(3));
 
-			return string.Format("{0}-{1}-{2}", numbers.Substring(0, 3), numbers.Substring(3, 3), numbers.Substring(6));
+				return string.Format("{0}-{1}-{2}", numbers.Substring(0, 3), numbers.Substring(3, 3), numbers.Substring(6));
+			}
 		}
 
 		public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
