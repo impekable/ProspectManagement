cfbe3bbd73c1838d0b17c895aa900d4acd01b2b6
danszot22 <danszot@hotmail.com>
2020-05-27 15:16:30 -0400

    Add BubbleCell image set.

---
+++ BubbleCell.designer.cs
@@ -0,0 +1,18 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+
+namespace ProspectManagement.iOS.Views
+{
+    [Register("BubbleCell")]
+    partial class BubbleCell
+    {
+        void ReleaseDesignerOutlets()
+        {
+        }
+    }
+}
